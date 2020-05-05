24d47e806cd85f00eae7564cc4f94533d34ab80f
Tor-Aksel Solberg <torakselsolberg@gmail.com>
2020-05-05 10:26:21 +0200

    Ingredients api now working

--- app/core/tests/test_ingredients_api.py
+++ app/core/tests/test_ingredients_api.py
@@ -74,10 +74,9 @@ class PrivateIngredientsApiTests(TestCase):
         ).exists()
         self.assertTrue(exists)
 
-
     def test_create_igredient_invalid(self):
         """Test creating invalid ingredient fails"""
-        payload = {'name':''}
+        payload = {'name': ''}
         res = self.client.post(INGREDIENTS_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
